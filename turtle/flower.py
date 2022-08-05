055925ae9cb3f7c28ae3b184b43e0236d4a8d7f8
ChoSooBeen <100505030+ChoSooBeen@users.noreply.github.com>
2022-08-05 15:07:30 +0900

    Update flower.py

--- turtle/flower.py
+++ turtle/flower.py
@@ -1,6 +1,7 @@
 import turtle as t
 import random
 
+#꽃 색갈 채우기
 def func_fillcolor():
     t.colormode(255)
     r = random.randint(0, 255)
@@ -8,6 +9,7 @@ def func_fillcolor():
     b = random.randint(0, 255)
     t.fillcolor(r, g, b)
 
+    #꽃잎 하나 그리기
 def func_leaf(size):
     for j in range(2):
         for i in range(15):
@@ -15,7 +17,7 @@ def func_leaf(size):
             t.right(360/60)
         t.right(90)
     
-
+#꽃 그리기
 def func_flower(leafs, size):
     func_fillcolor()
     t.begin_fill()
@@ -24,6 +26,7 @@ def func_flower(leafs, size):
         t.right(360/leafs) 
     t.end_fill()      
 
+    #좌클릭시 실행하는 
 def leftclick(x, y):
     t.penup()
     t.goto(x, y)
