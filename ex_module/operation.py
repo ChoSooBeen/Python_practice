50f977d56b1c5167dde6fc39d9a4c393eeaa18bd
ChoSooBeen <100505030+ChoSooBeen@users.noreply.github.com>
2022-08-08 14:01:29 +0900

    Update operation.py

--- ex_module/operation.py
+++ ex_module/operation.py
@@ -10,6 +10,6 @@ def func_mult(a, b):
 def func_div(a, b):
     return a / b
 
-print('연산자') #이 파일을 실행하거나 사용할 경우 무조건 나타남!
+print('연산자') #이 파일을 실행하거나 사용(import)할 경우 무조건 나타남!
 if __name__ == '__main__': #이 파일을 실행할 때만 나타남!!
     print('연산자 함수가 들어있는 파일이다.')
