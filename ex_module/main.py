6f603b734fc07692f1c17af3055a52332b1438e5
ChoSooBeen <100505030+ChoSooBeen@users.noreply.github.com>
2022-08-08 10:35:02 +0900

    Add files via upload

---
+++ ex_module/main.py
@@ -0,0 +1,13 @@
+from operation import *
+
+a = func_add(10, 5)
+print(a)
+
+a = func_sub(10, 5)
+print(a)
+
+a = func_mult(10, 5)
+print(a)
+
+a = func_div(10, 5)
+print(a)
---
+++ ex_module/operation.py
@@ -0,0 +1,11 @@
+def func_add(a, b):
+    return a + b
+
+def func_sub(a, b):
+    return a - b
+
+def func_mult(a, b):
+    return a * b
+
+def func_div(a, b):
+    return a / b
